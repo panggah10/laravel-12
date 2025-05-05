9cc895cb5910669136c77661d85472ee6a3dffb1
Panggah Widiandana <117331216+panggah10@users.noreply.github.com>
2025-05-05 10:39:13 +0700

    Update 050525

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 //import model product
 use App\Models\Product;
-
+// Redirect Respon
+use Illuminate\Http\RedirectResponse;
+// Request
+use Illuminate\Http\Request;
 //import return type View
 use Illuminate\View\View;
 
@@ -22,5 +25,35 @@ class ProductController extends Controller
 
         //render view with products
         return view('products.index', compact('products'));
+    }
+
+    public function create(): View
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        //validasi form
+        $request->validate([
+            'image'         => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        //create product
+        Product::create([
+            'image'         => $request->image->hashName(),
+            'title'         => $request->title,
+            'description'   => $request->description,
+            'price'         => $request->price,
+            'stock'         => $request->stock,
+
+        ]);
+
+        //redirect to index
+        return redirect()->route('products.index')->with(['success' => 'Data Berhasil Disimpan !']);
     }
 }
